4ae084bdfb06ff560040eaaec984ed0db9fb6ee5
PAULO SILAS DA SILVA SANTOS <3276.343@edu.fb.org.br>
2024-05-10 21:45:17 -0300

    co9

--- venv/cadastro.py
+++ venv/cadastro.py
@@ -1,64 +1,106 @@
-#fase 1- conexão
-
+# fase 1 - conexão
 import pymysql
 import os
 
 conexao = pymysql.connect(
-    host = 'localhost',
+    host='localhost',
     user='root',
-    password = 'fbradesco',
+    password='1234',
     database='dbloja'
-    
+   )
 
-)
 with conexao:
     with conexao.cursor() as cursor:
-    
-        TABLE_NAME='produto'
-
+        TABLE_NAME = 'produto'
+        #criação de uma tabela
         cursor.execute(
-            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}('
-            'id INT AUTO_INCREMENT PRIMARY KEY,'
-            'nome VARCHAR(50) NOT NULL,'
-            'valor double(10,2) NOT NULL,'
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT AUTO_INCREMENT PRIMARY KEY , '
+            'nome VARCHAR(50) NOT NULL, '
+            'valor DOUBLE(10,2) NOT NULL,'
             'quantidade INT NOT NULL'
-            
             ')'
         )
     conexao.commit()
 
     with conexao.cursor() as cursor:
-        TABLE_NAME='cliente'
+        TABLE_NAME = 'cliente'
+        #criação de uma tabela
         cursor.execute(
-            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}('
-            'id INT AUTO_INCREMENT PRIMARY KEY,'
-            'nome VARCHAR(50) NOT NULL,'
-            'cpf VARCHAR(11),'
-            'telefone VARCHAR(20),'
-            'email varchar(50)'
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT AUTO_INCREMENT PRIMARY KEY , '
+            'nome VARCHAR(50) NOT NULL, '
+            'cpf VARCHAR(11) ,'
+            'telefone VARCHAR(20) ,'
+            'email VARCHAR(50)'
             ')'
         )
     conexao.commit()
 
-    #LISTANDO 
-    with conexao.cursor() as cursor:
-        TABLE_NAME= input("Digite o nome da tabela : ")
+    op = input("1 - cadastro de produto\n2 - Cadastro de cliente\n3 - Selecionar um produto : ")
+    if op == '1':
+        #LISTANDO A BAGAÇA
+        with conexao.cursor() as cursor:
+            print("INICIANDO A TABELA")
+            TABLE_NAME = input("Digite o nome da tabela :").lower()
+            
+            sql =f'SELECT * FROM {TABLE_NAME}'
+            cursor.execute(sql)
+            resposta = cursor.fetchall()
+            print(resposta)
 
-        sql = f'SELECT * FROM {TABLE_NAME}'
+            #inserindo dados em produto
+            nome = input("Digite o nome do produto : ").upper()
+            valor = float(input("Digita o valor do produto :"))
+            quantidade = int(input("Digite a quantidade em estoque do produto : "))
+            dados = (nome,valor,quantidade)
+            sql = f'INSERT INTO {TABLE_NAME} (nome, valor, quantidade) VALUES (%s,%s,%s)'
+            cursor.execute(sql,dados)
+            conexao.commit()
 
-        cursor.execute(sql)
-        
-        resposta = cursor.fetchall()
-
-        print(resposta)
-
-        #inserindo dados em produto
-        sql = f'INSERT INTO produto(nome,valor,quantidade) values("CAMISA",50.50,10)'
-        cursor.execute(sql)
+            sql =f'SELECT * FROM {TABLE_NAME}'
+            cursor.execute(sql)
+            resposta = cursor.fetchall()
+            
+            for linha in resposta:
+                print(linha)
         conexao.commit()
-        
-        sql = f'SELECT * FROM {TABLE_NAME}'
-        cursor.execute(sql)
-        resposta = cursor.fetchall()
-        print(resposta)
+    elif op == '2':
+        with conexao.cursor() as cursor:
+            #inserindo dados em cliente
+            sql =f'SELECT * FROM {TABLE_NAME}'
+            cursor.execute(sql)
+            resposta = cursor.fetchall()
+            print(resposta)
+
+            TABLE_NAME = 'cliente'
+            nome = input("Digite o nome do cliente : ").upper()
+            cpf = input("Digite o CPF : ")
+            telefone = input("Digite o telefone : ")
+            email = input("Digite o email : ")
+            dados = (nome, cpf, telefone, email)
+            sql = f'INSERT INTO {TABLE_NAME} (nome, cpf, telefone, email) VALUES (%s,%s,%s,%s)'
+            cursor.execute(sql,dados)
+            conexao.commit()
+
+            sql =f'SELECT * FROM {TABLE_NAME}'
+            cursor.execute(sql)
+            resposta = cursor.fetchall()
+            
+            for linha in resposta:
+                print(linha) 
         conexao.commit()
+    elif op == '3':
+         with conexao.cursor() as cursor:
+            print("LISTANDO PRODUTOS")
+            TABLE_NAME = 'produto'
+            
+            nome = input('Digite o nome do produto :')
+            dados = (nome)
+            sql =f'SELECT * FROM {TABLE_NAME} WHERE nome=%s'
+            cursor.execute(sql,dados)
+            resposta = cursor.fetchall()
+            print(resposta)
+    else:
+        print("Opção invalida")
+
